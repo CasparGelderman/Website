cdecaeb45f2ecb84fb9733724f6c1ce7e28eb39a
SaraLiWijnia <saraliwynia@gmail.com>
2019-01-03 09:11:22 +0100

    .
    .

--- php.php
+++ php.php
@@ -1,5 +1,15 @@
-rgsrg
 
-<?php
-echo("HELLO");
-?>
+<?php 
+
+
+function footer() {
+  $footer = <<<FOOTER 
+  <footer>
+  <p> &copy; Caspar Gelderman en Sara Li Wijnia (2018)</p>
+</footer>
+FOOTER;
+
+return $footer;
+}
+
+?> 

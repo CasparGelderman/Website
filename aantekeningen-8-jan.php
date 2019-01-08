b3a0454ba421242c9e395446e0a35ed758d52011
SaraLiWijnia <saraliwynia@gmail.com>
2019-01-08 10:26:29 +0100

    .
    .

---
+++ aantekeningen-8-jan.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>$Title$</title>
+</head>
+<body>
+$END$
+
+<?php
+echo '<pre>';
+print_r(PDO::getAvailableDrivers());
+echo '</pre>'
+
+?>
+
+
+</body>
+</html>

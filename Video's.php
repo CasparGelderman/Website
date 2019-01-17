d955c1654ddaa9d182bccc268bf3f97cb5230a6d
SaraLiWijnia <saraliwynia@gmail.com>
2019-01-17 11:48:20 +0100

    video pagina css fix niet af
    als het klein is, moet het nog gefixt worden plus wil niet langs elkaar

--- Video's.php
+++ Video's.php
@@ -21,36 +21,30 @@ include 'includes\mainnavigatie.html';
         </div>
 
         </section>
-        <!-- <h3 id="OptellenAftrekken">Optellen en aftrekken</h3>
-        <p> Rekenen begint altijd met het leren om op te tellen en af te trekken, en daarom hebben wij hieronder een
-            aantal filmpjes voor jou om dit te leren!
-        </p>
-    </section>
-    <article>
-        <h5> a </h5>
-        <iframe src="https://www.youtube.com/embed/xEvMr4i5c4E">
-        </iframe>
-        <iframe src="https://www.youtube.com/embed/PZlUk3pvFd8">
-        </iframe>
-        <iframe src="https://www.youtube.com/embed/qh8fRZc4EBw">
-        </iframe>
-    </article>
-
 <?php
 include_once 'functies/functies.php';
 
 $datavideos = readMoviesFromDatabase();
     foreach ($datavideos as $records) {
-     $html = '   <section class="section-videos"> 
-        <h3 class="section-videos-h3">' . $records['titel'] . '</h3>
-            <iframe src=' . $records['link']. '>
+     $html = '   
+   
+     <article class="sectionvideos2">  
+
+     <section class="section-videos"> 
+        <h3>' . $records['titel'] . '</h3>
+            <iframe src= "' . $records['link']. '">
             </iframe>
-            <p>'.  $records['poster'] . '</p> 
-            <p>' .   $records['rubriek']  . ' </p> 
-            <p>' .  $records['gepubliceerd'] . '<p> 
-            <br> 
-            <p class="samenvatting">' .  $records['samenvatting'] . '</p>
-            </section> ';
+            <br>
+            <p class="samenvatting-videos">' .  $records['samenvatting'] . '</p>
+
+            <div class="tekstvideos"> 
+            <p>onderwerp: '.   $records['rubriek']  . ' </p> 
+            <p>'.  $records['gepubliceerd'] . '<p> 
+            </div> 
+
+            </section> <br>
+            </article>
+             ';
 
             echo $html;
     }
